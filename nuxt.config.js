8063d2dc64d907696cad5aaeb5333a7bf2d298da
samuelgoddard <hello@samgoddard.co.uk>
2019-07-23 09:13:30 +0100

    testig SPA mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   env: {
     DATO_KEY: process.env.DATO_KEY,
   },
-  mode: 'universal',
+  mode: 'spa',
   head: {
     title: process.env.npm_package_name || '',
     htmlAttrs: {
